93e1f691b3ce52869b408ce3fc38fc3c0361a052
SrShelo <correodeshelo@gmail.com>
2026-04-03 23:03:23 -0300

    updated the "update" command, now full written in bash.

--- common_scripts/update-arch.sh
+++ common_scripts/update-arch.sh
@@ -1,13 +1,12 @@
-#!/bin/fish
+#!/bin/bash
+PKG_MANAGERS=(pacman yay snap flatpak)
 
-set PKG_MANAGERS pacman yay snap flatpak
+BAD='bad'
+NICE='ok'
 
-set BAD 'bad'
-set NICE 'ok'
-
-set HELP_PAGE "
+HELP_PAGE="\
 A user definied script for updating the sistem with defined pkg managers.
-Script file: $(status filename)
+Script file: $0
 Usage:
     update
     update [OPTION]
@@ -17,98 +16,104 @@ Usage:
     -h, -?, --help      print this help page
 "
 
-# PACKAGE MANAGER FUNCTIONS
-function func_pacman
+func_pacman () {
     sudo echo "pacman:" | lolcat --seed=-25
     sudo pacman -Syyu --noconfirm
-end
+}
 
-function func_yay
+func_yay () {
     echo "yay:" | lolcat --seed=-15
     yay -Syyu --noconfirm
-end
+}
 
-function func_snap
+func_snap () {
     echo "snap:" | lolcat --seed=8
     sudo snap refresh
-end
+}
 
-function func_flatpak
+func_flatpak () {
     echo "flatpak:" | lolcat --seed=90
     flatpak update --noninteractive
-end
-###
+}
 
-function check_internet
+check_internet () {
     echo -n 'Checking internet connection... '
-    if ping -c2 archlinux.org > /dev/null
+    if ping -c2 archlinux.org > /dev/null ; then
         echo 'OK' | lolcat -S -10
     else
         echo 'BAD' | lolcat -S 80
         exit 1
-    end
-end
+    fi
+}
 
-function print_summary
+print_summary () {
     echo "System update status summary:" | lolcat
-    set summary ''
-    for the_command in $argv
-        set summary "$summary\n$the_command: $$the_command"
-    end
-    set summary $(echo $summary | sed 's/\\\n//')
-    string unescape $summary
+    summary=''
+    for the_command in "$@"
+    do
+        summary="$summary
+$the_command: ${!the_command}"
+    done
+    summary=${summary:1}
+    echo "$summary"
+    # summary=$(echo $summary | sed 's/\\\n//')
+    # string unescape $summary
     notify-send -t 10000 'System update status summary' "$summary" -u low
-end
+}
 
-if test -z $argv[1]
+if [ ! "$1" ]; then
     # Case: update without specifying pkg managers
     check_internet
     sudo echo 'Starting to update the system... ' | lolcat
-    for pkgman in $PKG_MANAGERS
-        if command -s $pkgman > /dev/null
-            set $pkgman $BAD
-            func_$pkgman && set $pkgman $NICE
-        end
-    end
+    for pkgman in "${PKG_MANAGERS[@]}"
+    do
+        if [ $(command -v $pkgman) ]; then
+            eval $pkgman=$BAD
+            func_$pkgman && eval $pkgman=$NICE
+        fi
+    done
     echo
-    print_summary $PKG_MANAGERS
+    print_summary "${PKG_MANAGERS[@]}"
 else
     # Check if the arguments are subcommands
-    switch $argv[1]
-        case -l --list
-            for pkgman in $PKG_MANAGERS
+    case "$1" in
+        -l | --list)
+            for pkgman in "${PKG_MANAGERS[@]}"
+            do
                 echo $pkgman
-            end
+            done
             exit 0
-        case -h -? --help
-            echo -n $HELP_PAGE
+            ;;
+        -h | -? | --help)
+            echo -n "$HELP_PAGE"
             exit 0
-    end
+            ;;
+    esac
     # If not, they must be pkg managers in the list
     # Check if the specified pkg managers are registered and
     # their commands are installed.
-    for arg in $argv
-        if contains $arg $PKG_MANAGERS
-            if not command -s $arg > /dev/null
+    for arg in "$@"
+    do
+        if echo "$@" | grep $arg > /dev/null; then
+            if [ ! $(command -v $arg) ]; then
                 echo -n ERROR: | lolcat
                 echo "'$arg' is not a command installed" | lolcat
                 echo "Remember to register package managers writing their COMMANDS (without arguments) in the variable 'PKG_MANAGERS' in $(status filename)"
                 exit 1
-            end
+            fi
         else
             echo -n 'ERROR: ' | lolcat
             echo "'$arg' is not registered as package manager in $(status filename)" | lolcat
             exit 1
-        end
-    end
+        fi
+    done
     check_internet
     echo 'Starting to update specified pkg managers...' | lolcat
-    for a_command in $argv
-        set $a_command $BAD
-        func_$a_command && set $a_command $NICE
-    end
-    # Update specified pkg managers
+    for arg in "$@"
+    do
+        eval $arg=$BAD
+        func_$arg && eval $arg=$NICE
+    done
     echo
-    print_summary $argv
-end
-
+    print_summary "$@"
+fi
